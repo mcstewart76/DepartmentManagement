1bfe81b3da4381e454a613ba20564cb657948be7
mcstewart76 <mcstewart76@gmail.com>
2022-02-02 12:40:28 -0500

    Issues with exporting functions, all functions now to be used in server file instead

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+//const mysql = require('mysql2');
+
 require('dotenv').config();
 
 let sequelize;
@@ -10,12 +12,26 @@ if (process.env.MYSQL_URL) {
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    {
+    
+    { logging: false,
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: 3306,
+      
     }
+    
   );
 }
+// const mysql = require("mysql2");
+// const connection = mysql.createConnection(
+//     {
+//       host: 'localhost',
+//       user: 'root',
+//       password: 'rooter',
+//       database: 'management_db'
+//     },
+//     console.log(`Connected to the management database.`)
+//   );
 
-module.exports = sequelize;
+  module.exports = sequelize;
+
--- server.js
+++ server.js
@@ -1,22 +1,52 @@
-const express = require('express');
+//const express = require('express');
 const sequelize = require('./config/connection');
-const question = require('./lib/questions');
-const models = require('./models');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+//const connector = require('sequelize');
+//const mysql = require('mysql2')
+
+
+
+//Imports functions from other files
+ const question = require('./lib/questions');
+// const adding = require('./lib/adding')
+// const view = require('./lib/views');
+
+ const models = require('./models');
+
+// For seeding only
+const Employee = require('./models/Employee');
+const Department = require('./models/Department');
+
+const seedDepartmentData = require('./seeds/departmentSeedData.json');
+
+
+
 
 //const mysql = require('mysql2');
+//const app = express();
+//const PORT = process.env.PORT || 3001;
 
-const PORT = process.env.PORT || 3001;
-const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
 
-function  serverStart() {
- return sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on ' + PORT));
-}); 
+const seedDatabase = () => {
+  return sequelize.sync({ force: true }).then(() => {
+    Department.bulkCreate(seedDepartmentData)
+  })
 };
+  // sequelize.sync({ force: true })
+  //  .then(() => {
+  // // Department.bulkCreate(seedData).then(() =>{
+  //  //app.listen(PORT, () => console.log('Now listening on ' + PORT)); 
+ // Department.bulkCreate(seedDepartmentData)
+   
+  // }) 
+
+  
+
+
+
 
 
   
@@ -24,52 +54,127 @@ function  serverStart() {
 // % App Starter %
 //////////////////////////////////////////////////////////////////////
 
+// initialQuestions();
+
+  
+  const initialQuestions =async() =>{
+    return inquirer
+       .prompt(question.startupQuestion)
+          .then(function(data) {
+          
+              switch (data.introQuestion) {
+                  case "View All Employees":
+                    viewEmployees();
+                    break;
+                  case "Add Employee":
+                    addEmployee();
+                    break;
+                  case "Update Employee Role":
+                    updateEmployee();
+                    break;
+                  case "View All Roles":
+                    viewRoles();
+                    break;
+                  case "Add Role":
+                    addRole();
+                    break;
+                  case "View All Departments":
+                    viewDepartments();
+                    break;
+                  case "Add Department":
+                  
+                   addDepartment(); 
+                  
+                  
+                  
+                  
+                    break;
+                  case "Quit":
+                    console.log("\n Goodbye! \n")
+                    
+                    process.exit(0);
+              
+              }
+        
+          })
 
 
-  function initialQuestions() {
-    
-    return   inquirer
-       .prompt(question.startupQuestionPrompt())
-       .then((data) => {
-      switch (data['introQuestion']){
-          case "View All Employees":
-            viewEmployees();
-            break
-          case "Add Employee":
-            addEmployee();
-            break
-          case "Update Employee Role":
-            updateEmployee();
-            break
-          case "View All Roles":
-            viewRoles();
-            break
-          case "Add Role":
-            addRole();
-            break
-          case "View All Departments":
-            viewDepartments();
-            break
-          case "Add Department":
-            addDepartment();
-            break
-          case "Quit":
-            console.log("\n Goodbye! \n")
-            process.exit(0);
-
-      }
-    });
-  }
+  
   
 
-   const runner = async () => {
-    const serverStarter = await serverStart();
+ 
+  //   // const seedDatabase = () => {
+  //   // return  sequelize.sync({ force: false }).then(() => {
+  //   //     })
+  //   //    .then(() => {
+  //   //       console.log("Completed Department Seeding")
+  //   //    Department.bulkCreate(seedData)
+      
+  //   //    });
+      
+  //   // // }
+  
+  
+
+
+// //module.exports = {initialQuestions}
+
+// // ///////////////////////////////////////////////////////
+// // %% App Questions %%
+// /////////////////////////////////////////////////////
+  function addRole() {
+    inquirer
+      .prompt(question.addRolePrompt())
+        .then(function(data) {
+            
+            //console.log(data['departmentName'])
+            //add to schema here
+              console.log(data.roleName)
+              console.log(data.roleSalary)
+              //query departments
+              console.log(data.roleDepartment)
+            
+        })
+        
+  };
+
+//Add Department Function
+  function addDepartment () {
+    inquirer
+    .prompt(question.addDepartment)
+        .then(function(data) {
+            console.log(data.departmentName + ' was added!')
+        //     console.log(data['departmentName'])
+        //    // add to schema here
+        //     sequelize.sync({
+        //         force: false
+        //     }).then(function(){
+                Department.create({
+        
+                    "name": data.departmentName
+                    
+                })
+                
+        //     }).then(function(){
+        //      return  
+        //     })
+            
+
+              
+            
+        }).then(function(){
+          initialQuestions();
+        })
+      
+  };
+  
+}
+const runner = async () => {
+    //const serverStarter = await serverStart();
+    seedDatabase();
     console.log("\n\n\n")
     console.log("Initilizing Application!")
     const docmake = await initialQuestions();
-    
+    console.log(docmake)
     }
     runner();
-
-    module.exports = {initialQuestions}
-  
